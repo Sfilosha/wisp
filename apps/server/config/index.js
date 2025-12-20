dded6a54ca276bdd40cb45078a1fae8653c1d596
Sfilosha <sfilosha@gmail.com>
2025-12-20 19:22:42 +0200

    secrets fix

--- apps/server/config/index.js
+++ apps/server/config/index.js
@@ -1,17 +1,15 @@
-import fs from 'fs';
-import path from 'path';
-import dotenv from 'dotenv';
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  figmaApiKey: process.env.FIGMA_API_KEY,
-  figmaFileKey: 'ktUGhzlx0XSuKCf85SHvhb',
   PORT: 9000,
   httpsOptions: {
-    key: fs.readFileSync(path.resolve('./localhost-key.pem')),
-    cert: fs.readFileSync(path.resolve('./localhost.pem')),
+    key: fs.readFileSync(path.resolve("./localhost-key.pem")),
+    cert: fs.readFileSync(path.resolve("./localhost.pem")),
   },
-  outputDir: '../../packages/design-system/src',
-  tokensFilePath: '../../packages/tokens/tokens.css',
-  variablesFilePath: '../../packages/tokens/variables.json',
+  outputDir: "../../packages/design-system/src",
+  tokensFilePath: "../../packages/tokens/tokens.css",
+  variablesFilePath: "../../packages/tokens/variables.json",
 };
