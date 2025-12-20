ceb0599d97687b2846cc6f8bdc9513225fb559f4
Sfilosha <sfilosha@gmail.com>
2025-12-20 19:33:16 +0200

    fix package and import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button } from "../packages/design-system/src/button/button";
+import { Input } from "../packages/design-system/src/Input/Input";
+import { Textarea } from "../packages/design-system/src/Textarea/Textarea";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  function submitForm(e) {
+    e.preventDefault();
+    console.log(`Details: ${name}, ${message}`);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Contact Form</h1>
+      <p>Enter your details and press submit</p>
+      <form className="card" onSubmit={submitForm}>
+        <Input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="First Name"
+        ></Input>
+        <Textarea
+          value={message}
+          placeholder="Your message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button style="Primary" size="Medium" align="center" value={`Submit`} />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// <div>
+// <input type="checkbox" id="agreement" name="termsofuse"></input>
+// <label for="agreement">I agree with Terms of Use </label>
+// </div>
+// <Input
+// value={surname}
+// onChange={(e) => setSurname(e.target.value)}
+// placeholder='Last Name'></Input>
+
+// const [surname, setSurname] = useState('');
